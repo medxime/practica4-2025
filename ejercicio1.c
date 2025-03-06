afdb86ea96b16b7933564eb2f4e46a99684ff15b
medxime <anmedina1024@gmail.com>
2025-03-06 14:04:04 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,16 @@
+//Medina Vega ANel Ximena
+//ejercicio1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int a=3;
+    int b=2;
+    if(a>b)
+    {
+        printf("a es mayor\n");
+    }
+    return 0;
+}
